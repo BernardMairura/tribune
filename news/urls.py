7143782d063039857ffac3655954616359cae170
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-11-11 17:09:46 +0300

    uploading images

--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,8 @@ from .import views
 
 
 urlpatterns=[ 
-    re_path(r'^$',views.news_of_day,name='newsToday'),
+    path('',views.news_of_day,name='newsToday'),
+    path('search/', views.search_results, name='search_results'),
     re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
 
 ]
